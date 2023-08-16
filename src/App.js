b6ab1d1963322dc431742b5a21fcc527d1b12edc
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-17 00:42:31 +0530

    cart template complted

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
 	{
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
 	{
 		path: "/signup",
 		element: <SignupPage />,
+	},
+	{
+		path: "/cart",
+		element: <CartPage />,
 	},
 ]);
 
