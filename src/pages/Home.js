6d3a71139dd79f142a9158e26289be225de0fbaa
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-17 22:11:48 +0530

    some changes in navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../features/navbar/Nvavbar";
+import Navbar from "../features/navbar/Navbar";
 import Product from "../features/product/Product";
 
 const Home = () => {
