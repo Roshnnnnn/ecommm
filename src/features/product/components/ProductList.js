290f61371c3944d70da5b9fab26f06673e0d7f84
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-19 00:53:06 +0530

    some changes in product

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -1,5 +1,6 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchAllProductAsync, selectAllProducts } from "../ProductSlice";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
@@ -68,6 +69,10 @@ export default function Product() {
 	const dispatch = useDispatch();
 	const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
 	const products = useSelector(selectAllProducts);
+
+	useEffect(() => {
+		dispatch(fetchAllProductAsync());
+	}, [dispatch]);
 
 	return (
 		<div>
@@ -329,49 +334,61 @@ export default function Product() {
 										<div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
 											<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
 												{products.map((product) => (
-													<div
+													<Link
+														to={`/product-detail/${product.id}`}
 														key={product.id}
-														className="group relative border-solid border-2 p-2 border-gray-200 "
 													>
-														<div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
-															<img
-																src={product.thumbnail}
-																alt={product.title}
-																className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-															/>
-														</div>
-														<div className="mt-4 flex justify-between">
-															<div>
-																<h3 className="text-sm text-gray-700">
-																	<a href={product.thumbnail}>
-																		<span
-																			aria-hidden="true"
-																			className="absolute inset-0"
-																		/>
-																		{product.title}
-																	</a>
-																</h3>
-																<p className="mt-1 text-sm text-gray-500">
-																	<StarIcon className="w-6 h-6 inline" />
-																	<span className="align-bottom">
-																		{product.rating}
-																	</span>
-																</p>
+														<div className="group relative border-solid border-2 p-2 border-gray-200">
+															<div className="min-h-60 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
+																<img
+																	src={product.thumbnail}
+																	alt={product.title}
+																	className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+																/>
 															</div>
-															<div>
-																<p className="text-sm font-medium text-gray-900">
-																	$
-																	{Math.round(
-																		product.price *
-																			(1 - product.discountPercentage / 100)
-																	)}
-																</p>
-																<p className="text-sm block line-through font-medium text-gray-400">
-																	${product.price}
-																</p>
+															<div className="mt-4 flex justify-between">
+																<div>
+																	<h3 className="text-sm text-gray-700">
+																		<div href={product.thumbnail}>
+																			<span
+																				aria-hidden="true"
+																				className="absolute inset-0"
+																			/>
+																			{product.title}
+																		</div>
+																	</h3>
+																	<p className="mt-1 text-sm text-gray-500">
+																		<StarIcon className="w-6 h-6 inline"></StarIcon>
+																		<span className=" align-bottom">
+																			{product.rating}
+																		</span>
+																	</p>
+																</div>
+																<div>
+																	<p className="text-sm block font-medium text-gray-900">
+																		${product.discountPrice}
+																	</p>
+																	<p className="text-sm block line-through font-medium text-gray-400">
+																		${product.price}
+																	</p>
+																</div>
 															</div>
+															{product.deleted && (
+																<div>
+																	<p className="text-sm text-red-400">
+																		product deleted
+																	</p>
+																</div>
+															)}
+															{product.stock <= 0 && (
+																<div>
+																	<p className="text-sm text-red-400">
+																		out of stock
+																	</p>
+																</div>
+															)}
 														</div>
-													</div>
+													</Link>
 												))}
 											</div>
 										</div>
