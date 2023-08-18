ce3e58ac2b0eb1f6409ac67288ea4d7a9f9c4d6c
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-18 21:32:03 +0530

    checkout template completed

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -52,10 +52,10 @@ const Checkout = () => {
 		<div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
 			<div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
 				<div className="lg:col-span-3">
-					<form className="bg-white px-5 mt-12">
+					<form className="bg-white px-5 py-12 mt-12">
 						<div className="space-y-12">
 							<div className="border-b border-gray-900/10 pb-12">
-								<h2 className="text-base font-semibold leading-7 text-gray-900">
+								<h2 className="text-2xl font-semibold leading-7 text-gray-900">
 									Personal Information
 								</h2>
 								<p className="mt-1 text-sm leading-6 text-gray-600">
@@ -210,6 +210,20 @@ const Checkout = () => {
 										</div>
 									</div>
 								</div>
+								<div className="mt-6 flex items-center justify-end gap-x-6">
+									<button
+										type="button"
+										className="text-sm font-semibold leading-6 text-gray-900"
+									>
+										Reset
+									</button>
+									<button
+										type="submit"
+										className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+									>
+										Add Address
+									</button>
+								</div>
 							</div>
 
 							<div className="border-b border-gray-900/10 pb-12">
@@ -304,21 +318,6 @@ const Checkout = () => {
 								</div>
 							</div>
 						</div>
-
-						<div className="mt-6 flex items-center justify-end gap-x-6">
-							<button
-								type="button"
-								className="text-sm font-semibold leading-6 text-gray-900"
-							>
-								Cancel
-							</button>
-							<button
-								type="submit"
-								className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-							>
-								Save
-							</button>
-						</div>
 					</form>
 				</div>
 				<div className="lg:col-span-2">
@@ -390,12 +389,12 @@ const Checkout = () => {
 								Shipping and taxes calculated at checkout.
 							</p>
 							<div className="mt-6">
-								<a
-									href="#"
+								<Link
+									to={"/pay"}
 									className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
 								>
-									Checkout
-								</a>
+									Pay Now
+								</Link>
 							</div>
 							<div className="mt-6 flex justify-center text-center text-sm text-gray-500">
 								<p>
