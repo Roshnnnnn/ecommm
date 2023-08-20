6613aa25a939671b5346ccf1907475fd50a42606
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-08-21 01:02:49 +0530

    line-through
    productlist m line through tk ho gya

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -338,15 +338,15 @@ export default function Product() {
 														<div key={product.id} className="group relative">
 															<div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
 																<img
-																	src={product.imageSrc}
-																	alt={product.imageAlt}
+																	src={product.thumbnail}
+																	alt={product.title}
 																	className="h-full w-full object-cover object-center lg:h-full lg:w-full"
 																/>
 															</div>
 															<div className="mt-4 flex justify-between">
 																<div>
 																	<h3 className="text-sm text-gray-700">
-																		<a href={product.href}>
+																		<a href={product.thumbnail}>
 																			<span
 																				aria-hidden="true"
 																				className="absolute inset-0"
@@ -355,12 +355,24 @@ export default function Product() {
 																		</a>
 																	</h3>
 																	<p className="mt-1 text-sm text-gray-500">
-																		{product.color}
+																		<StarIcon className="w-6 h-6 inline" />
+																		<span className="align-bottom">
+																			{product.rating}
+																		</span>
 																	</p>
 																</div>
-																<p className="text-sm font-medium text-gray-900">
-																	{product.price}
-																</p>
+																<div>
+																	<p className="text-sm block font-medium text-gray-900">
+																		$
+																		{Math.round(
+																			product.price *
+																				(1 - product.discountPercentage / 100)
+																		)}
+																	</p>
+																	<p className="text-sm block line-through font-medium text-gray-400">
+																		${product.price}
+																	</p>
+																</div>
 															</div>
 														</div>
 													</Link>
