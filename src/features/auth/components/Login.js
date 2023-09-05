eaab89dbc19831ce34f0e3f424da070972a2d10b
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-05 20:49:24 +0530

    now working on auth part

--- src/features/auth/components/Login.js
+++ src/features/auth/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import { selectCount } from "../authSlice";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 export default function Login() {
