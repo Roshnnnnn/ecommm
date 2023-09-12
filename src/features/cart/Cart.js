6f42c48c8b2f7b9294fc5295b77eabd7503084c7
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-12 20:31:30 +0530

    productDetails
    getting error
    when you empty the cart and again adding the item is giving error

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 // import { selectCount } from "./cartSlice";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import {
 	deleteItemFromCartAsync,
 	selectItems,
@@ -30,6 +30,7 @@ const Cart = () => {
 
 	return (
 		<div>
+			{!items.length && <Navigate to={"/"} replace={true} />}
 			<div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
 				<div className="border-t border-gray-200 px-4 py-6 sm:px-6">
 					<h1 className="text-4xl my-5 font-bold tracking-tight text-gray-900">
