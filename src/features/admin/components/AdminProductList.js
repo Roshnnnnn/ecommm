9f82f46bbe4b17ed3cfd917f306277ad1e55a928
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-22 01:18:43 +0530

    working on productForm

--- src/features/admin/components/AdminProductList.js
+++ src/features/admin/components/AdminProductList.js
@@ -375,12 +375,12 @@ export default function AdminProductList() {
 
 								{/* Product grid */}
 								<div className="lg:col-span-3">
-									<button
-										type="submit"
+									<Link
+										to="/admin/product-form"
 										className="rounded-md mx-10 my-5 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 									>
 										Add New Product
-									</button>
+									</Link>
 									<ProductGrid products={products} />
 								</div>
 								{/*Product grid end */}
