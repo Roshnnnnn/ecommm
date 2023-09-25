94d6f0a821a2ed5beb62e6bb500168f7474bea72
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-09-26 02:34:47 +0530

    working on auth

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,7 +1,7 @@
 // A mock function to mimic making an async request for data
 export function createUser(userData) {
 	return new Promise(async (resolve) => {
-		const response = await fetch("http://localhost:8080/users", {
+		const response = await fetch("http://localhost:8080/auth/signup", {
 			method: "POST",
 			body: JSON.stringify(userData),
 			headers: { "content-type": "application/json" },
@@ -13,19 +13,17 @@ export function createUser(userData) {
 
 export function checkUser(loginInfo) {
 	return new Promise(async (resolve, reject) => {
-		const email = loginInfo.email;
-		const password = loginInfo.password;
-		const response = await fetch("http://localhost:8080/users?email=" + email);
-		const data = await response.json();
-		console.log({ data });
-		if (data.length) {
-			if (password === data[0].password) {
-				resolve({ data: data[0] });
-			} else {
-				reject({ message: "User not found" });
-			}
-		} else {
-			reject({ message: "user not found" });
+		try {
+			const response = await fetch("http://localhost:8080/auth/login", {
+				method: "POST",
+				body: JSON.stringify(loginInfo),
+				headers: { "content-type": "application/json" },
+			});
+			const data = await response.json();
+			console.log({ data });
+			resolve({ data });
+		} catch (err) {
+			reject({ err });
 		}
 	});
 }
