53b2c475ab9e151eaa617ed011d0916df1efd509
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-19 12:44:41 +0530

    some changes

--- src/pages/ProductDetailPage.js
+++ src/pages/ProductDetailPage.js
@@ -1,16 +1,16 @@
-import React from "react";
-import Navbar from "../features/navbar/Navbar";
+import NavBar from "../features/navbar/Navbar";
 import ProductDetail from "../features/product/components/ProductDetail";
 import Footer from "../features/common/Footer";
 
-const ProductDetailPage = () => {
+function ProductDetailPage() {
 	return (
 		<div>
-			<Navbar />
-			<ProductDetail />
-			<Footer />
+			<NavBar>
+				<ProductDetail></ProductDetail>
+			</NavBar>
+			<Footer></Footer>
 		</div>
 	);
-};
+}
 
 export default ProductDetailPage;
