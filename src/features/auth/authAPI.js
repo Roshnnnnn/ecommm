4a38b2dafc059dc2cc5bcf9bb95fd9577000e0c8
Roshnnnnn <139259259+Roshnnnnn@users.noreply.github.com>
2023-10-31 00:15:41 +0530

    updated

--- src/features/auth/authAPI.js
+++ src/features/auth/authAPI.js
@@ -1,109 +1,63 @@
+
 export function createUser(userData) {
-	return new Promise(async (resolve) => {
-		const response = await fetch("http://localhost:8080/auth/signup", {
-			method: "POST",
-			body: JSON.stringify(userData),
-			headers: { "content-type": "application/json" },
-		});
-		const data = await response.json();
-		// TODO: on server it will only return some info of user (not password)
-		resolve({ data });
-	});
+  return new Promise(async (resolve) => {
+    const response = await fetch('http://localhost:8080/auth/signup', {
+      method: 'POST',
+      body: JSON.stringify(userData),
+      headers: { 'content-type': 'application/json' },
+    });
+    const data = await response.json();
+    // TODO: on server it will only return some info of user (not password)
+    resolve({ data });
+  });
 }
 
 export function loginUser(loginInfo) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await fetch("http://localhost:8080/auth/login", {
-				method: "POST",
-				body: JSON.stringify(loginInfo),
-				headers: { "content-type": "application/json" },
-			});
-			if (response.ok) {
-				const data = await response.json();
-				resolve({ data });
-			} else {
-				const error = await response.text();
-				reject(error);
-			}
-		} catch (error) {
-			reject(error);
-		}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch('http://localhost:8080/auth/login', {
+        method: 'POST',
+        body: JSON.stringify(loginInfo),
+        headers: { 'content-type': 'application/json' },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        resolve({ data });
+      } else {
+        const error = await response.text();
+        reject(error);
+      }
+    } catch (error) {
+      reject( error );
+    }
 
-		// TODO: on server it will only return some info of user (not password)
-	});
+    // TODO: on server it will only return some info of user (not password)
+  });
 }
 
 export function checkAuth() {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await fetch("http://localhost:8080/auth/check");
-			if (response.ok) {
-				const data = await response.json();
-				resolve({ data });
-			} else {
-				const error = await response.text();
-				reject(error);
-			}
-		} catch (error) {
-			reject(error);
-		}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const response = await fetch('http://localhost:8080/auth/check');
+      if (response.ok) {
+        const data = await response.json();
+        resolve({ data });
+      } else {
+        const error = await response.text();
+        reject(error);
+      }
+    } catch (error) {
+      reject( error );
+    }
 
-		// TODO: on server it will only return some info of user (not password)
-	});
+    // TODO: on server it will only return some info of user (not password)
+  });
 }
+
 
 export function signOut(userId) {
-	return new Promise(async (resolve) => {
-		// TODO: on server we will remove user session info
-		resolve({ data: "success" });
-	});
-}
-
-export function resetPasswordRequest(email) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await fetch(
-				"http://localhost:8080/auth/reset-password-request",
-				{
-					method: "POST",
-					body: JSON.stringify({ email }),
-					headers: { "content-type": "application/json" },
-				}
-			);
-			if (response.ok) {
-				const data = await response.json();
-				resolve({ data });
-			} else {
-				const error = await response.text();
-				reject(error);
-			}
-		} catch (error) {
-			reject(error);
-		}
-	});
-}
-
-export function resetPassword(data) {
-	return new Promise(async (resolve, reject) => {
-		try {
-			const response = await fetch(
-				"http://localhost:8080/auth/reset-password",
-				{
-					method: "POST",
-					body: JSON.stringify(data),
-					headers: { "content-type": "application/json" },
-				}
-			);
-			if (response.ok) {
-				const data = await response.json();
-				resolve({ data });
-			} else {
-				const error = await response.text();
-				reject(error);
-			}
-		} catch (error) {
-			reject(error);
-		}
-	});
+  return new Promise(async (resolve) => {
+    // TODO: on server we will remove user session info
+    resolve({ data: 'success' });
+  });
 }
